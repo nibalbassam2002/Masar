6831b636f4d45d1868c04e4c16f046577c4a78ba
nibal <nebal.ps@gmail.com>
2026-02-04 15:43:45 +0200

    edit1

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
         Gate::before(function ($user, $ability) {
         return $user->isSuperAdmin() ? true : null;
     });
+     if (app()->environment('production')) {
+        URL::forceScheme('https');
+    }
     }
     
 }
